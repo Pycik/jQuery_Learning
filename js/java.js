00cb88c68d310ce6c0654e3ca66ed1960455c8ab
Russel Crow <pyccok@gmail.com>
2017-08-03 19:51:31 +0300

    jQuery_learning

--- js/java.js
+++ js/java.js
@@ -39,3 +39,47 @@ $(document).ready(function() {
 	$(".text h2").addClass("menu");
 
 });
+
+function blocker() {
+	$(".row.block h2").addClass("blue");
+}
+$(document).ready(blocker);
+
+$(document).ready(function() {
+	console.log('Hello');
+	console.log('25');
+	console.log($('blue').length);
+
+});
+
+$(function() {
+	$(".row.block p").css("color","purple");
+	$(".row.block p").css("font-size","20px");
+	$(".row.block p").css("border","dotted 2px #aaa");
+});
+
+$(function() {
+	$(".row.block h3").html("Hello It's Me 0_x");
+	$(".row.block h3").css("color","red");
+	$(".row.block h3").css("border","solid 5px #d2a5e3");
+
+});
+
+$("#btn_start").click(function() {
+	$("#letters")
+	.css("background","#111")
+	.css("color","#fff")
+	.css("font-size","20px")
+});
+
+$("#btn_reset").click(function() {
+	location.reload();
+});
+
+$("#btn_delete").click(function() {
+	$("#letters").hide();
+});
+
+$("#btn_delete_all").click(function() {
+	$("html").hide();
+});
