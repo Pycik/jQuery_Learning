736a0288e2c97b1685e396eaeb94cf5401949a81
Russel Crow <pyccok@gmail.com>
2017-08-03 17:59:02 +0300

    jQuery_learning

--- js/java.js
+++ js/java.js
@@ -1,3 +1,41 @@
+$("#test_hide").click(function(){
+    $("#test_hide").hide();
+});
+
 $("#test_alert").click(function(){
     alert("The button was clicked.");
+});
+
+
+
+$("#alert_h2").click(function() {
+var test = $(".text h2").html();
+alert(test);
+
+});
+
+$("#alert_length").click(function() {
+var length = $(".text p").length;
+alert(length);
+
+});
+
+$(document).ready(function() {
+	$(".text p").addClass("black");
+
+});
+
+$(document).ready(function() {
+	$(".black_button button").addClass("black");
+
+});
+
+$(document).ready(function() {
+	$(".nav.navbar-nav ").addClass("menu");
+
+});
+
+$(document).ready(function() {
+	$(".text h2").addClass("menu");
+
 });
