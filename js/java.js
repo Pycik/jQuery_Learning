1a9e6f0f6761f27b983ce0adff1acd2fb7240be9
Russel Crow <pyccok@gmail.com>
2017-08-08 19:19:28 +0300

    jQuery_learning

--- js/java.js
+++ js/java.js
@@ -155,10 +155,10 @@ $(".anim2").click(function() {
 });
 
 
-$("#stick").click(function(){
-	$("#stick").css("color","red");
-	$(".text_none").slideDown("slow");
-});
+// $("#stick").click(function(){
+// 	$("#stick").css("color","red");
+// 	$(".text_none").slideDown("slow");
+// });
 
 // function changeClass() {
 // 	$(this).prev().toggleClass("active")
@@ -181,5 +181,74 @@ $("#stick").click(function(){
 
 	
 $("article h2").click(function(){
+	$("#stick").css("color","red");
 	$(this).next().slideToggle();		
 });
+
+$("#hider_text").click(function(){
+	$(".container p").hide("slow");
+});
+
+$("#show_text").click(function(){
+	$(".container p").show("fast");
+});
+
+$("#hide_show").click(function(){
+	$(".container p").toggleClass("hider");
+});
+
+$("#circle_1").click(function() {
+	$(this).fadeOut(1600);
+	$(this).fadeIn(1600);
+});
+
+$("#circle_2").click(function() {
+	$(this).fadeTo("slow", 0.2);	
+});
+
+
+// $("#circle_3").animate ({
+// 	"opacity" : "hide",			
+// });
+
+// $("#circle_3").animate ({
+// 	"opacity" : "show",			
+// });
+
+// $("#circle_3").animate ({"opacity" : "-=0.9",})
+// 			  .animate ({"height" : "+=150px",})
+// 			  .animate ({"width" : "+=250px",})
+// 			  .animate ({"opacity" : "1",})
+// 			  .animate ({"height" : "80px",})
+// 			  .animate ({"width" : "80px",})
+
+$("#circle_3").click(function() {
+
+	$(this).animate ({"opacity" : "-=0.9"})
+		   .animate ({"height" : "+=150px"})
+		   .animate ({"width" : "+=250px"})
+		   .animate ({"opacity" : "1"})
+		   .animate ({"height" : "80px"}).stop()
+		   .animate ({"width" : "80px"},{queue:false})
+		   .animate ({"height" : "180px"})
+		   .animate ({"height" : "100px"})
+});
+// $.fx.off = true
+
+var myDiv = $('<div id="my" class="page">Hello my Friend </div>').appendTo("header,#letters").css({color:"red",
+																						  fontWeight:"bold",
+																						  fontSize:"20px"
+	});
+
+var myDiv =$('<div>Hello</div>').attr({'id':'my','class':'page'}).appendTo("header");
+
+// var myDiv = document.createElement('div');
+// 	myDiv.id = "my";
+// 	myDiv.className = "page";
+
+$(".circle").after("<hr/>").before("<hr/>");
+$("<hr/>").insertAfter("h2");
+$("footer").append("<hr/>");
+$("<p>Last</p>").appendTo("#last_p p");
+$("<p>Last of Last</p>").prependTo("#last_p h2");
+
